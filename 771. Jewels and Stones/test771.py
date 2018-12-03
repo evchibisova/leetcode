e59c2290d12e18ff496d383371358ba26adfb9d2
echibisova <echibisova@mail.ru>
2018-12-03 21:25:05 +0300

    add test to jewels and stones

--- 771. Jewels and Stones/test771.py
+++ 771. Jewels and Stones/test771.py
@@ -6,5 +6,20 @@ class TestTask771(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
+    def test_from_leetcode_1(self):
+        self.assertEqual(self.solution.numJewelsInStones("aA", "aAAbbbb"), 3)
+
+    def test_from_leetcode_2(self):
+        self.assertEqual(self.solution.numJewelsInStones("z", "ZZ"), 0)
+
+    def test_all_jewels(self):
+        self.assertEqual(self.solution.numJewelsInStones("z", "zzzzz"), 5)
+
+    def test_empty_jewels(self):
+        self.assertEqual(self.solution.numJewelsInStones("", "abcdef"), 0)
+
+    def test_empty_stones(self):
+        self.assertEqual(self.solution.numJewelsInStones("aA", ""), 0)
+
     def test_empty(self):
         self.assertEqual(self.solution.numJewelsInStones("", ""), 0)
