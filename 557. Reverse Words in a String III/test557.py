40edadd700dc24ae4d7013d06525c71f42276ef6
echibisova <echibisova@mail.ru>
2018-12-11 10:18:02 +0300

    test: modify test for task 557: union two tests

--- 557. Reverse Words in a String III/test557.py
+++ 557. Reverse Words in a String III/test557.py
@@ -12,10 +12,8 @@ class TestSolution(unittest.TestCase):
     def test_from_leetcode_1(self):
         self.assertEqual("s'teL ekat edoCteeL tsetnoc", self.s.reverseWords("Let's take LeetCode contest"))
 
-    def test_is_integer(self):
-        with self.assertRaises(AttributeError, msg="it's a number, not a string"):
+    def test_inp_is_not_a_string(self):
+        with self.assertRaises(AttributeError, msg="input is not a string"):
             self.s.reverseWords(1)
-
-    def test_is_list(self):
-        with self.assertRaises(AttributeError, msg="it's a number, not a string"):
-            self.s.reverseWords([1,2,3])
+            self.s.reverseWords(1.0)
+            self.s.reverseWords([1, 2, 3])
